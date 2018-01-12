ab00e50a3da8ab41f51f8e7a177887e24d1efc17
LevelUp! <levelup@teleopti.com>
2018-01-12 15:22:15 +0100

    Adding ensure recurring jobs to superflow if retries is supported

--- src/Options.java
+++ src/Options.java
@@ -3,4 +3,5 @@ package com.teleopti.wfm.developer.tools;
 public class Options{
     public boolean RestoreToLocalFlow = false;
     public boolean EtlEnsureRecurringJobs = false;
+    public boolean EtlEnsureRecurringJobsWithRetries = false;
 }
--- src/actions/SuperFlow.java
+++ src/actions/SuperFlow.java
@@ -3,10 +3,7 @@ package com.teleopti.wfm.developer.tools.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.util.IconLoader;
-import com.teleopti.wfm.developer.tools.CommandRunner;
-import com.teleopti.wfm.developer.tools.OptionsReader;
-import com.teleopti.wfm.developer.tools.PathMaker;
-import com.teleopti.wfm.developer.tools.ResourceExtractor;
+import com.teleopti.wfm.developer.tools.*;
 
 public class SuperFlow extends AnAction {
 
@@ -17,12 +14,22 @@ public class SuperFlow extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         ResourceExtractor.Extract("SuperFlow.bat", PathMaker.InTemp("SuperFlow.bat"));
-        if (new OptionsReader().Read().RestoreToLocalFlow){
-            CommandRunner.StartInCommandWindow(e,
-                    PathMaker.InRepo(""),
-                    PathMaker.InTemp("SuperFlow.bat"),
-                    "/flow"
-            );
+        Options options = new OptionsReader().Read();
+        if (options.RestoreToLocalFlow) {
+            if (options.EtlEnsureRecurringJobsWithRetries) {
+                CommandRunner.StartInCommandWindow(e,
+                        PathMaker.InRepo(""),
+                        PathMaker.InTemp("SuperFlow.bat"),
+                        "/flow",
+                        "/EnsureRecurringJobs"
+                );
+            } else {
+                CommandRunner.StartInCommandWindow(e,
+                        PathMaker.InRepo(""),
+                        PathMaker.InTemp("SuperFlow.bat"),
+                        "/flow"
+                );
+            }
         } else {
             CommandRunner.StartInCommandWindow(e,
                     PathMaker.InRepo(""),
