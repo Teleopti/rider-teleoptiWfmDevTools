5fec1d2cbdba7a197acac0d311dc904689411ab6
LevelUp! <levelup@teleopti.com>
2018-02-16 13:02:03 +0100

    No more stack overflow?

--- src/actions/TeleoptiMenu.java
+++ src/actions/TeleoptiMenu.java
@@ -2,6 +2,7 @@ package com.teleopti.wfm.developer.tools.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.Presentation;
 
 public class TeleoptiMenu extends DefaultActionGroup {
 
@@ -9,10 +10,18 @@ public class TeleoptiMenu extends DefaultActionGroup {
         super("TeleoptiMenu", true);
     }
 
+    private boolean _inUpdate = false;
+
     @Override
     public void update(AnActionEvent event) {
-        if (!event.getPresentation().isEnabled())
-            event.getPresentation().setEnabled(true);
-        event.getPresentation().setText("Teleopti");
+        if (_inUpdate)
+            return;
+        _inUpdate = true;
+        Presentation presentation = event.getPresentation();
+        if (!presentation.isEnabled())
+            presentation.setEnabled(true);
+        presentation.setText("Teleopti");
+        _inUpdate = false;
     }
+
 }
