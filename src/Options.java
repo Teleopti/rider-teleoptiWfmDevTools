ef88ed0374fb0d80953b20001dd6f510f6cf919b
LevelUp! <levelup@teleopti.com>
2018-02-16 10:28:41 +0100

    Ignore any newly added properties not implemented yet

--- src/Options.java
+++ src/Options.java
@@ -1,5 +1,8 @@
 package com.teleopti.wfm.developer.tools;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Options{
     public boolean RestoreToLocalFlow = false;
     public boolean EtlEnsureRecurringJobs = false;
