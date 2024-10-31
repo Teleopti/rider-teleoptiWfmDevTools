88772876ea05310d831779bb31c5d3f6c2f362c8
Mathias Stenbom <mathias.stenbom@calabrio.com>
2024-10-31 16:39:55 +0100

    Remove unused legacy json properties

--- src/Options.java
+++ src/Options.java
@@ -4,16 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Options {
-    public boolean RestoreToLocalFlow = false;
-    public boolean EtlEnsureRecurringJobs = false;
-    public boolean EtlEnsureRecurringJobsWithRetries = false;
-    public boolean NpmRunRtaTest = false;
-    public boolean ToolsFlowWithoutSQLCMD = false;
-    public boolean ToolsFlowWithFixMyConfig = false;
-    public boolean ToolsFlowWithInfraTestConfigWithOptionalToggleMode = false;
-    public boolean BatflowThatWorks = false;
-    public boolean NpmRunStartAlpha = false;
-    public boolean NpmStartAndFriends = false;
     public OptionsAction[] TeleoptiMenu;
     public OptionsAction[] NavigationToolBar;
 }
